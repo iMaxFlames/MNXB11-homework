a19541d2786d1a55b7a58cf2f6f6af11116769cf
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-15 12:39:16 +0200

    Added ex1 folder and updated types.cxx

--- tutorial7/types.cxx
+++ tutorial7/types.cxx
@@ -2,8 +2,8 @@
 int main()
 {
     int age = 20;
-    bool is_adult = age > 18;
-    std::vector<int> list_of_integers{1, 2, 3};
+    bool isAdult = age > 18;
+    std::vector<int> listOfIntegers{1, 2, 3};
     // Casting or converting from one type to another *intentinoally*
     const int i{static_cast<int>(3.9999999999)};
     // i = 4; // gives compiler error
