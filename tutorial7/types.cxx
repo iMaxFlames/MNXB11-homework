99f1f58d3ab8654d6dc10b26973ba24c75546d20
Ayush Chakraborty <chak.polly37@gmail.com>
2023-10-25 22:06:05 +0200

    Updated types.cxx and control_flow.cxx

--- tutorial7/types.cxx
+++ tutorial7/types.cxx
@@ -1,10 +1,7 @@
 #include <vector>
+#include <string>
+#include <iostream>
 int main()
 {
-    int age = 20;
-    bool isAdult = age > 18;
-    std::vector<int> listOfIntegers{1, 2, 3};
-    // Casting or converting from one type to another *intentinoally*
-    const int i{static_cast<int>(3.9999999999)};
-    // i = 4; // gives compiler error
+    
 }
